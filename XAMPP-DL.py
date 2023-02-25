c6e25defbc3ab4dc88d02ace381cab39f314e651
LordFluffyJr <jrsedorejr@gmail.com>
2023-02-25 08:45:28 -0500

    Create new file
    Added python script to download xampp installer

---
+++ XAMPP-DL.py
@@ -0,0 +1,7 @@
+import gdown
+
+url = "https://drive.google.com/uc?id=1l_5RK28JRL19wpT22B-DY9We3TVXnnQQ"
+output = "fcn8s_from_caffe.npz"
+gdown.download(url, output, quiet=False)
+
+# USED TO DOWNLOAD XAMPP INSTALLER FROM GOOGLE DRIVE
